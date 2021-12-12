419ad50a8e7977a6903943c6e987926ae71f67f9
Michael Wang <michael.yj.wang@gmail.com>
2021-12-12 12:53:36 -0500

    format

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -15,15 +15,6 @@ class DeadlinedReminder(Iterable, ABC):
     def is_due(self):
         pass
 
-class DateReminder(DeadlinedReminder):
-    def __init__(self, text, date):
-        self.date = parse(date, dayfirst = True)
-        self.text = text
-    def is_due(self):
-        return self.date <= datetime.now()
-    def __iter__(self):
-        return iter([self.text, self.date.isoformat()])
-
     def __subclasshook__(cls, subclass):
         if cls is not DeadlinedReminder:
             return NotImplemented
@@ -32,3 +23,12 @@ class DateReminder(DeadlinedReminder):
         if not all (attr_in_hierarchy(attr) for attr in ('__iter__', 'is_due')):
             return NotImplemented
         return True
+
+class DateReminder(DeadlinedReminder):
+    def __init__(self, text, date):
+        self.date = parse(date, dayfirst = True)
+        self.text = text
+    def is_due(self):
+        return self.date <= datetime.now()
+    def __iter__(self):
+        return iter([self.text, self.date.isoformat()])
