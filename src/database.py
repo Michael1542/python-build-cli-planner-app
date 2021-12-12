2c2ad1506b3b01423cb2071f732bedd30cbdcc64
Michael Wang <michael.yj.wang@gmail.com>
2021-12-12 12:31:48 -0500

    3

--- src/database.py
+++ src/database.py
@@ -1,6 +1,7 @@
 import csv
-from src.reminder import PoliteReminder
-from src.deadlined_reminders import DateReminder
+from typing import Type
+from src.deadlined_reminders import DeadlinedReminder
+
 
 def list_reminders():
     f = open("reminders.csv", "r")
@@ -14,9 +15,11 @@ def list_reminders():
                 print(e.ljust(32), end=' ')
         print()
 
-def add_reminder(text, date):
+def add_reminder(text, date, ReminderClass):
 
-    reminder =DateReminder(text, date)
+    if not issubclass(ReminderClass, DeadlinedReminder):
+        raise TypeError("Invalid Reminder Class")
+    reminder =ReminderClass(text, date)
     
     with open('reminders.csv', 'a+', newline='\n') as file:
         writer = csv.writer(file)
